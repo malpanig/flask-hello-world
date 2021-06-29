30ed0a67f98ff769aaa6f266b2b17beeb3db38b4
malpanig <70102817+malpanig@users.noreply.github.com>
2021-06-29 19:22:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
 from fuzzywuzzy import fuzz
+import numpy as np
+import pypyodbc
+import pandas as pd
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/dashboard/<name>')
+def dashboard(name):   
+    return 'welcome %s' % name
